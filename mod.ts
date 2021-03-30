6ffcd64f514ad2efa28282c2ce73cb3645642289
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-03-30 10:14:48 +0900

    use sift

--- mod.ts
+++ mod.ts
@@ -1,13 +1,5 @@
-import { serve } from "https://deno.land/std@0.91.0/http/mod.ts";
+import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
 
-const server = serve({ port: 80 });
-
-for await (const req of server) {
-  await req.respond({
-    status: 200,
-    headers: new Headers({
-      "content-tyoe": "text/plain",
-}),
-    body: "200 OK",
-})
-}
+serve({
+  "/": () => new Response("200 OK", {headers: {"content-type": "text/plain; charset=utf-8"}}),
+});
